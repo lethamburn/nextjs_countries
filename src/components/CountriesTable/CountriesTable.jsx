8e03f6f0e148c3bbad88588d2823790701e23cde
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-11-29 11:54:55 +0100

    FEAT CountriesTable sort & arrows

--- src/components/CountriesTable/CountriesTable.jsx
+++ src/components/CountriesTable/CountriesTable.jsx
@@ -1,19 +1,37 @@
 import styles from "./CountriesTable.module.css";
+import KeyboardArrowDownRounded from "@material-ui/icons/KeyboardArrowDownRounded";
 
+const orderBy = (countries, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? 1 : -1
+    );
+  }
+  if (direction === "desc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? -1 : 1
+    );
+  }
+  return countries;
+};
 const CountriesTable = ({ countries }) => {
-    console.log(countries)
+  const orderedCountries = orderBy(countries, "desc");
+
   return (
     <div>
       <div className={styles.heading}>
         <button className={styles.heading_name}>
           <div>Name</div>
+          <KeyboardArrowDownRounded />
         </button>
         <button className={styles.heading_population}>
           <div>Population</div>
+
+          <KeyboardArrowDownRounded />
         </button>
       </div>
-      
-      {countries.map((country) => (
+
+      {orderedCountries.map((country) => (
         <div className={styles.row} key={country.name}>
           <div className={styles.name}>{country.name.common}</div>
           <div className={styles.population}>{country.population}</div>
