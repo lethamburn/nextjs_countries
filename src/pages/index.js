264ca92585f0d93806d8d447a4bedf64d3e6a5f3
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-12-02 09:50:05 +0100

    FEAT keyword in SearchInput & filteredCountries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import CountriesTable from "../components/CountriesTable/CountriesTable.jsx";
 import Layout from "../components/Layout/Layout.jsx";
 import SearchInput from "../components/SearchInput/SearchInput.jsx";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.common.toLowerCase().includes(keyword) ||
+      country.name.official.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput placeholder="Filter by Name, Region or Subregion" />
+      <SearchInput
+        placeholder="Filter by Name, Region or Subregion"
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
